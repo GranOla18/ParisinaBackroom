57f9c6e2b7448cd3d076f9d18afa31a082595786
GranOla18 <80288848+GranOla18@users.noreply.github.com>
2023-03-16 21:30:05 -0600

    Added Charge behaviour and limit of flash
    The ChargeBattery coroutine charges every 5 seconds 10% of the battery until it reaches 100%, and added a limit of flashes that doesn't let to use the flash unless it has 40%+ of battery.

--- Assets/Scripts/Player/Flashlight/FlashlightBehaviour.cs
+++ Assets/Scripts/Player/Flashlight/FlashlightBehaviour.cs
@@ -7,14 +7,17 @@ public class FlashlightBehaviour : MonoBehaviour
     Light flLight;
     bool isOn;
     public float chargeFlash;
+    public float flBattery;
     bool canFlash;
     float flashTime;
+    bool isCharging;
 
     // Start is called before the first frame update
     void Start()
     {
         flLight = GetComponent<Light>();
         isOn = true;
+        flBattery = 100;
     }
 
     // Update is called once per frame
@@ -38,10 +41,13 @@ public class FlashlightBehaviour : MonoBehaviour
         {
             chargeFlash += Time.deltaTime;
 
-            if(chargeFlash >= 1)
+            if(chargeFlash >= 1 && flBattery >= 40)
             {
                 canFlash = true;
-                Debug.Log("cola lampara");
+            }
+            else
+            {
+                canFlash = false;
             }
         }
 
@@ -53,12 +59,13 @@ public class FlashlightBehaviour : MonoBehaviour
             if (canFlash)
             {
                 StartCoroutine(FlashRoutine());
-
+                if (!isCharging)
+                {
+                    StartCoroutine(ChargeBattery());
+                }
             }
 
             canFlash = false;
-            //StartCoroutine(FlashRoutine());
-            Debug.Log("se levanto");
         }
     }
 
@@ -66,8 +73,20 @@ public class FlashlightBehaviour : MonoBehaviour
     {
         flLight.intensity = 8;
         flLight.spotAngle = 90;
+        flBattery -= 40;
         yield return new WaitForSeconds(0.35f);
         flLight.intensity = 2;
         flLight.spotAngle = 60;
+    }
+
+    IEnumerator ChargeBattery()
+    {
+        while (flBattery < 100)
+        {
+            isCharging = true;
+            flBattery += 10;
+            yield return new WaitForSeconds(5f);
+        }
+        isCharging = false;
     }
 }
