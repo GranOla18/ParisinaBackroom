db06340bb8bb0a096569b852b930c60483509541
GranOla18 <80288848+GranOla18@users.noreply.github.com>
2023-03-23 14:46:45 -0600

    Started Main Menu functionality
    Added functionality to the Inicio Button and Salir Button.
    
    Created the ADMIN game object and added there the CursorBehaviour.cs. Also created this GameObject in MainMap and added the cursor script.

--- Assets/Scripts/UI/CursorBehaviour.cs
+++ Assets/Scripts/UI/CursorBehaviour.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CursorBehaviour : MonoBehaviour
 {
+    Scene scene;
+
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Confined;
+        scene = SceneManager.GetActiveScene();
+        if (scene.buildIndex  == 0)
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+        else if(scene.buildIndex == 1)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
     }
 
     // Update is called once per frame
