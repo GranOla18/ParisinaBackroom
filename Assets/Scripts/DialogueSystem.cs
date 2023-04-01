f34846b2d031482756126cfdbd18dfccaacd2be9
GranOla18 <80288848+GranOla18@users.noreply.github.com>
2023-03-31 19:50:44 -0600

    Made Dialogue System

---
+++ Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+
+    public static DialogueSystem instance;
+
+    private Queue<string> sentences;
+
+    #region Singleton
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+        
+    }
+
+    [ContextMenu("Start Conversation")]
+    public void StartDialogue(Dialogue dialogue)
+    {
+        //Debug.Log("Starting conversation with " + dialogue.name);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    [ContextMenu("Next Sentence")]
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        //Debug.Log(sentence);
+        //dialogueText.text = sentence;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForEndOfFrame();
+            //yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End conversation");
+    }
+}
