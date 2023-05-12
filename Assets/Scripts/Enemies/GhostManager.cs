e07082e1d1c214cc87282cb3d4d23c314b691cd4
GranOla18 <80288848+GranOla18@users.noreply.github.com>
2023-05-12 17:21:30 -0600

    Ghost General NavMesh
    Moves to a random position of the created array

--- Assets/Scripts/Enemies/GhostManager.cs
+++ Assets/Scripts/Enemies/GhostManager.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GhostManager : Interactable
 {
+    NavMeshAgent agent;
+    public Transform[] walkPoints;
+    public int walkToIdx;
+    public Vector3 target;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+
+    public void GotoNextPoint()
+    {
+        walkToIdx = Random.Range(1, 4);
+        agent.SetDestination(walkPoints[walkToIdx].position);
+        Debug.Log("Walking to " + walkToIdx);
     }
 
     public override void EnterTrigger()
