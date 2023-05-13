8e5cb2d5b25fd60918fea1352e8588e41de50c37
GranOla18 <80288848+GranOla18@users.noreply.github.com>
2023-05-12 20:06:55 -0600

    Added Settings Menu to Pause Menu

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -36,6 +36,7 @@ public class PauseMenu : MonoBehaviour
     public void HidePauseMenu()
     {
         pauseMenuCanvas.enabled = false;
+        settingsCanvas.enabled = false;
     }
 
     public void ShowSettings()
